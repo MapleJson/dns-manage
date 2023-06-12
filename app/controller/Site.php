596de03e477b16de231a2b5362d85c4c9bf7deb0
bigface <bigface@gitlab.com>
2023-06-12 18:06:32 +0800

    edit

--- app/controller/Site.php
+++ app/controller/Site.php
@@ -45,6 +45,9 @@ class Site extends AdminController
             $item->backend_domain_url = fix_url($item->backend_domain);
         }
         $domains = Domains::field('id, domain, site_id, remark')->select()->column(null, 'id');
+        foreach ($domains as &$domain) {
+            $domain->domain = fix_url($domain->domain);
+        }
         return $this->view('list', compact('list', 'siteStatus', 'domains', 'areas'));
     }
 
